de34356a5e066d07319c65cf457bac2fcf3bb9e3
acer-haitao <acer_yuhaitao@163.com>
2018-03-02 16:03:17 +0800

    更新数据库联合主键 防止重复插入数据（设置job,company等唯一键）insert or

--- 51job/51job.py
+++ 51job/51job.py
@@ -4,10 +4,11 @@
 # @Email   : acer_yuhaitao@163.com
 # @File    : 51job.py
 # @Software: PyCharm
-import urllib
 import re
 import sqlite3
 import sys
+import urllib
+
 reload(sys)
 sys.setdefaultencoding('utf8')#处理打印中文字体用Unicode编码
 
@@ -42,8 +43,10 @@ def data_to_sqlite(id,job,company,address,wages,date,jobname):
     将信息存储到数据库
     """
     db = sqlite3.connect("D:\Python-Test\WeiXin\db.sqlite3")
-    cursor = db.cursor()
-    sql = "insert into '51job'(job,company,address,wages,date,jobname) values (\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\");"%(job,company,address,wages,date,jobname)
+    cursor = db.cursor()  # OR IGNORE重复数据会跳过
+    sql = "insert  OR IGNORE into '51jobtest'(job,company,address,wages,date,jobname) values (\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\");" % (
+    job, company, address, wages, date, jobname)
+    # sql = "insert OR IGNORE  into '51jobtest'(job,company,address,wages,date,jobname) values (\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\");" % ("job", "company", "address", "wages", "date", "jobname")
     try:
         cursor.execute(sql)
         db.commit()
@@ -143,8 +146,10 @@ def one_job_get():
     """
     单个职位信息抓取
     """
+    jobname = u"华清远见"
+    urlstart = 'http://search.51job.com/list/010000%252C020000%252C030200%252C040000%252C200200,000000,0000,00,9,99,%25E5%258D%258E%25E6%25B8%2585%25E8%25BF%259C%25E8%25A7%2581,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
     # jobname = "python"
-    # urlstart = 'http://search.51job.com/list/010000,000000,0000,00,9,99,Python%25E5%25BC%2580%25E5%258F%2591%25E5%25B7%25A5%25E7%25A8%258B%25E5%25B8%2588,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
+    #urlstart = 'http://search.51job.com/list/010000,000000,0000,00,9,99,Python%25E5%25BC%2580%25E5%258F%2591%25E5%25B7%25A5%25E7%25A8%258B%25E5%25B8%2588,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
     # jobname = u"嵌入式"
     # urlstart = 'http://search.51job.com/list/010000,000000,0000,00,9,99,%25E5%25B5%258C%25E5%2585%25A5%25E5%25BC%258F%25E5%25BC%2580%25E5%258F%2591,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
     # jobname =u"云计算"
@@ -157,9 +162,10 @@ def one_job_get():
     # urlstart = 'http://search.51job.com/list/010000,000000,0000,00,9,99,%25E8%2587%25AA%25E5%258A%25A8%25E9%25A9%25BE%25E9%25A9%25B6,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=1&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
     #jobname =u"北上广深python"
     #urlstart = 'http://search.51job.com/list/010000%252C040000%252C020000%252C030200,000000,0000,00,9,99,python,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=1&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
-    jobname = u"BJ技术支持"
-    urlstart = 'http://search.51job.com/list/010000,000000,0000,00,9,99,%25E6%258A%2580%25E6%259C%25AF%25E6%2594%25AF%25E6%258C%2581,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
-
+    # jobname = u"BJ技术支持"
+    # urlstart = 'http://search.51job.com/list/010000,000000,0000,00,9,99,%25E6%258A%2580%25E6%259C%25AF%25E6%2594%25AF%25E6%258C%2581,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
+    # jobname = u"C#"
+    #urlstart = 'http://search.51job.com/list/010000,000000,0000,00,9,99,C%2523,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=1&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
     html = url_input(urlstart)#获取首页
     all_page_num = int(find_all_page(html))#从首页获取总共页数
     print("+++++++++++++++++%s++++++++++++++++++++" % (all_page_num))
